92e2bc18d20cd62ef27fef79bdb62fd2be1d344f
papettoTV <aegfrompsbt@gmail.com>
2021-10-07 22:04:17 +0900

    emit test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to Your Vue.js App" @emitmethod="onEmitMethod" />
 </template>
 
 <script>
@@ -8,6 +8,11 @@ import HelloWorld from "./components/HelloWorld.vue"
 
 export default {
   name: "App",
+  methods: {
+    onEmitMethod(e) {
+      console.log("onEmitMethod", e)
+    },
+  },
   components: {
     HelloWorld,
   },
