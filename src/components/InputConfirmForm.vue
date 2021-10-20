4e6afd346134bba5c3a1ea6673cbece782d67c0f
papettoTV <aegfrompsbt@gmail.com>
2021-10-20 21:45:59 +0900

    確認画面の改行対応

--- src/components/InputConfirmForm.vue
+++ src/components/InputConfirmForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>{{ body }}</p>
+  <nl2br tag="p" :text="body" class="foo bar" />
   <button @click="$emit('edit', body)">修正</button>
   <button @click="$emit('save', body)">保存</button>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
+import Nl2br from "vue3-nl2br"
 
-createApp(App).mount('#app')
+createApp(App)
+  .component("nl2br", Nl2br)
+  .mount("#app")
