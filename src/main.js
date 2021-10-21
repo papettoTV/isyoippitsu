0198e3d2a8322bdc44ce7d4bb34439f3b0c214fe
papettoTV <aegfrompsbt@gmail.com>
2021-10-21 21:46:36 +0900

    firebase setting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import Nl2br from "vue3-nl2br"
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz9xsxV903h5zq5oCNUXnn3Ld4o4oI2aA",
+  authDomain: "isyo-894d2.firebaseapp.com",
+  projectId: "isyo-894d2",
+  storageBucket: "isyo-894d2.appspot.com",
+  messagingSenderId: "274415468618",
+  appId: "1:274415468618:web:5634c12171e40bbb74e6b3",
+  measurementId: "G-8XE4XDBH4P",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 createApp(App)
   .component("nl2br", Nl2br)
