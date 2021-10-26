9c7971b45ea95df2089a4774a48ce173d0f7d6e8
papettoTV <aegfrompsbt@gmail.com>
2021-10-26 22:48:46 +0900

    oauth and add data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,8 +16,30 @@
 <script>
 import InputForm from "./InputForm.vue"
 import InputConfirmForm from "./InputConfirmForm.vue"
+// import { getAuth, signInWithPopup, TwitterAuthProvider } from "firebase/auth"
+import { initializeApp } from "firebase/app"
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+} from "firebase/firestore"
 
-import { getAuth, signInWithPopup, TwitterAuthProvider } from "firebase/auth"
+const firebaseConfig = {
+  apiKey: "AIzaSyCz9xsxV903h5zq5oCNUXnn3Ld4o4oI2aA",
+  authDomain: "isyo-894d2.firebaseapp.com",
+  projectId: "isyo-894d2",
+  storageBucket: "isyo-894d2.appspot.com",
+  messagingSenderId: "274415468618",
+  appId: "1:274415468618:web:5634c12171e40bbb74e6b3",
+  measurementId: "G-8XE4XDBH4P",
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+
+const db = getFirestore()
 
 export default {
   data() {
@@ -32,37 +54,11 @@ export default {
       this.isConfirm = true
       this.body = e
     },
-    onSave(body) {
+    async onSave(body) {
       console.log("onSave", body)
-      // 認証
-
-      const provider = new TwitterAuthProvider()
-
-      const auth = getAuth()
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-          // You can use these server side with your app's credentials to access the Twitter API.
-          // const credential = TwitterAuthProvider.credentialFromResult(result)
-          // const token = credential.accessToken
-          // const secret = credential.secret
-
-          // The signed-in user info.
-          const user = result.user
-          // ...
-          console.log(user)
-        })
-        .catch((error) => {
-          // // Handle Errors here.
-          // const errorCode = error.code
-          // const errorMessage = error.message
-          // // The email of the user's account used.
-          // const email = error.email
-          // // The AuthCredential type that was used.
-          // const credential = TwitterAuthProvider.credentialFromError(error)
-          // // ...
-          console.log(error)
-        })
+      oauth(body)
+      // update(body, "user_id")
+      read()
     },
     onEdit(body) {
       console.log("onEdit", body)
@@ -73,6 +69,53 @@ export default {
     InputForm,
     InputConfirmForm,
   },
+}
+
+function oauth(body) {
+  const provider = new TwitterAuthProvider()
+  const auth = getAuth()
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user
+      console.log(user, body)
+      // 保存完了UI
+      // showSaveComplete()
+      update(body, user.displayName)
+    })
+    .catch((error) => {
+      // // Handle Errors here.
+      // const errorCode = error.code
+      // const errorMessage = error.message
+      // // The email of the user's account used.
+      // const email = error.email
+      // // The AuthCredential type that was used.
+      // const credential = TwitterAuthProvider.credentialFromError(error)
+      // // ...
+      console.log(error)
+    })
+}
+
+async function update(body, user_id) {
+  console.log("update", body, user_id)
+
+  try {
+    await setDoc(doc(db, "isyos", user_id), {
+      body: body,
+      oauth_type: "twitter",
+      user_id: "user_id",
+      user_name: "user_name",
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function read() {
+  const querySnapshot = await getDocs(collection(db, "isyos"))
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data())
+  })
 }
 </script>
 
