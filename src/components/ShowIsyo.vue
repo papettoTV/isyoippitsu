3fca1cb30db4b067b76f54393d352cc168f1d06b
papettoTV <aegfrompsbt@gmail.com>
2021-12-23 23:30:39 +0900

    setup のsyntax sugar でasync await できた

--- src/components/ShowIsyo.vue
+++ src/components/ShowIsyo.vue
@@ -1,14 +1,19 @@
 <template>
-  <p>{{ userInfo.user_id }}</p>
+  <p>ShowIsyo</p>
+  <p>{{ userInfo }}</p>
 </template>
 
-<script async setup>
+<script setup>
 import { read } from "../libs/db.js"
 import { useRoute } from "vue-router"
 import { reactive } from "vue"
 
-const route = useRoute()
 let userInfo = reactive({})
+const route = useRoute()
 
-userInfo = await read(route.params.userId)
+const done = async function() {
+  const readResult = await read(route.params.userId)
+  userInfo.user_id = readResult.user_id
+}
+done()
 </script>
