95fca4a6062c304b873916febd9bd13920d90b2e
papettoTV <aegfrompsbt@gmail.com>
2022-01-04 22:03:22 +0900

    保存処理を追加し、外部化

--- src/components/EditIsyo.vue
+++ src/components/EditIsyo.vue
@@ -1,12 +1,13 @@
 <template>
   <textarea id="body" v-model="body"></textarea>
-  <button>編集完了する</button>
+  <button @click="onSave(body)">編集完了する</button>
 </template>
 
 <script setup>
 import { read } from "../libs/db.js"
 import { useRoute } from "vue-router"
 import { ref } from "vue"
+import { onSave } from "../libs/save.js"
 
 let body = ref("")
 const route = useRoute()
---
+++ src/libs/save.js
@@ -0,0 +1,15 @@
+import { read } from "../libs/db.js"
+import { oauth } from "../libs/oauth.js"
+
+export async function onSave(val) {
+  console.log("onSave", val)
+  await oauth(val, showSaveComplete)
+}
+
+export async function showSaveComplete(user) {
+  const userInfo = await read(user.displayName)
+  console.log("userInfo", userInfo)
+
+  // redirect show isyo
+  location.href = "/" + userInfo.user_id
+}
