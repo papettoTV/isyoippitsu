e61af138fca573584f0835c396b595e545f63671
papettoTV <aegfrompsbt@gmail.com>
2022-01-06 22:09:00 +0900

    InputForm を setup 構文にリファクタ

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -3,6 +3,7 @@
   <button @click="$emit('inputBody', body)">確認</button>
 </template>
 
+<!--
 <script>
 export default {
   emits: ["inputBody"],
@@ -13,6 +14,25 @@ export default {
   data() {
     return {
       body: this.inputedBody,
+    }
+  },
+}
+</script>
+-->
+<script>
+import { toRefs } from "vue"
+export default {
+  props: {
+    inputedBody: String,
+  },
+  emits: ["inputBody"],
+  setup(props) {
+    // console.log("inputedBody", props.inputedBody)
+
+    const { inputedBody } = toRefs(props)
+    // console.log("inputedBody", inputedBody.value)
+    return {
+      body: inputedBody.value,
     }
   },
 }
