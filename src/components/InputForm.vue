81c5c33608cd2c59f4a64e4540e32ce94934cc3c
papettoTV <aegfrompsbt@gmail.com>
2022-02-14 23:00:38 +0900

    InpuFormをちょっとだけQuasar化

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,6 +1,24 @@
 <template>
-  <textarea id="body" v-model="body"></textarea>
-  <button @click="$emit('inputBody', body)">確認</button>
+  <div class="row">
+    <div class="col">
+      <h3>遺書を書いてみませんか？</h3>
+      <p>
+        遺書を書くとしたら誰に書きますか？何を伝えますか？<br />
+        あなたが最後に、誰に何を伝えるか、その思いを一度形にしてみませんか？
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <q-input id="body" v-model="body" filled type="textarea" />
+      <q-btn
+        color="white"
+        text-color="black"
+        label="確認"
+        @click="$emit('inputBody', body)"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
