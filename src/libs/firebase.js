b8221a4d635176558a06abcdbba13f58aabd78f0
papettoTV <aegfrompsbt@gmail.com>
2022-04-07 22:46:37 +0900

    add app check

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app"
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check"
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
@@ -13,5 +14,17 @@ const firebaseConfig = {
 export function firebaseInit() {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig)
+
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // key is the counterpart to the secret key you set in the Firebase console.
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(
+      "6LfLelQfAAAAAFjI1QgxuVl_NFwsRwGmyf2vrnk2"
+    ),
+
+    // Optional argument. If true, the SDK automatically refreshes App Check
+    // tokens as needed.
+    isTokenAutoRefreshEnabled: true,
+  })
   return app
 }
